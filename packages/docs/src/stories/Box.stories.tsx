b60fd3ec9279d4c1f593b4f5f2133506ccf000a1
flvSantos15 <flvSantos300@gmail.com>
2023-02-22 22:30:23 -0300

    feat: box component

---
+++ packages/docs/src/stories/Box.stories.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+import type { Meta, StoryObj } from '@storybook/react'
+import { Box, BoxProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Surfaces/Box',
+  component: Box,
+  args: {
+    children: (
+      <>
+        <span>Testando o elemento Box</span>
+      </>
+    )
+  }
+} as Meta<BoxProps>
+
+export const Primary: StoryObj<BoxProps> = {}
