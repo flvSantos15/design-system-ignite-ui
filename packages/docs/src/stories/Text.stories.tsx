4a7baeb358c4c444229ec95cc913dc0606b5900d
flvSantos15 <flvSantos300@gmail.com>
2023-02-22 22:48:20 -0300

    feat: text component

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable prettier/prettier */
+import type { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children: 'Example text'
+  }
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong Text',
+    as: 'strong'
+  }
+}
