bcbe31c50a425541ccc01112ad4f14e93f35d2e3
flvSantos15 <flvSantos300@gmail.com>
2023-02-23 23:00:45 -0300

    feat: heading + avatar component

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable prettier/prettier */
+import type { Meta, StoryObj } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/flvSantos15.png',
+    alt: 'Flavio Santos'
+  }
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined
+  }
+}
